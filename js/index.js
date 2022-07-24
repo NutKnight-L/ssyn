f74b4e8e22221b86ae83459010402e6d43be7fec
NutKnight-L <2803793299@qq.com>
2022-07-24 21:15:37 +0800

    .

---
+++ js/index.js
@@ -0,0 +1,17 @@
+var footer = document.getElementById('footer');
+var afooter = footer.getElementsByTagName('a');
+//判断选中并高亮底部超链接
+for(var i=0;i<afooter.length;i++){
+    afooter[i].onclick = a_visited;
+}
+function a_visited(){
+    for(var i=0; i<afooter.length; i++){
+        if(afooter[i] === this){
+            afooter[i].className = "avisited";
+        }
+        else {
+            afooter[i].className = "";
+        }
+        
+    }
+}
